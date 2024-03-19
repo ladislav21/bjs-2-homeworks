b491435813e61c0ad4db58277244d368ea3f3734
ladislav Laptop <vladislavchs@internet.ru>
2024-03-19 13:58:49 +0300

    task 1.1 done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2 - 4*a*c;
+  let x1, x2;
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d))/(2*a);
+    x2 = (-b - Math.sqrt(d))/(2*a);
+    arr.push(x1, x2);
+  }
+  else if (d === 0) {
+    x1 = -b/(2*a);
+    arr.push(x1);
+  }
   
   return arr;
 }
