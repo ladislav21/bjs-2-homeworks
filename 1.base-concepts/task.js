6c0d43de3d3a6fc539d4a9c0a1e0ed9d9d5ff3dd
ladislav Laptop <vladislavchs@internet.ru>
2024-03-19 14:34:00 +0300

    task 1.2 done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,12 +4,12 @@ function solveEquation(a, b, c) {
   let d = b**2 - 4*a*c;
   let x1, x2;
   if (d > 0) {
-    x1 = (-b + Math.sqrt(d))/(2*a);
-    x2 = (-b - Math.sqrt(d))/(2*a);
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
   }
   else if (d === 0) {
-    x1 = -b/(2*a);
+    x1 = -b / (2 * a);
     arr.push(x1);
   }
   
@@ -17,5 +17,10 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyPercent = percent / 100 / 12;
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**countMonths) - 1)));
+  let totalPayment = monthlyPayment * countMonths;
+  totalPayment = Math.round(totalPayment * 100) / 100;
+  return totalPayment;
 }
