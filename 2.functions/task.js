6b215cee90c159ba7ff05df79a67b8c8df836623
ladislav Laptop <vladislavchs@internet.ru>
2024-03-23 13:28:31 +0300

    task 2.2.3 done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -31,15 +31,28 @@ function summElementsWorker(...arr) {
 function differenceMaxMinWorker(...arr) {
   let max = 0;
   let min = 0;
+
   if (arr.length > 0) {
     max = Math.max(...arr);
     min = Math.min(...arr);
   }
+
   return (max - min); 
 }
 
 function differenceEvenOddWorker(...arr) {
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+
+  return (sumEvenElement - sumOddElement);
 }
 
 function averageEvenElementsWorker(...arr) {
