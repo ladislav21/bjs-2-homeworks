a5feff7aff1f9e02fa600209a3c22e18e283126b
ladislav Laptop <vladislavchs@internet.ru>
2024-03-23 12:45:00 +0300

    task 2.1 done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,17 @@
 function getArrayParams(...arr) {
-
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = sum / arr.length;
+  avg = +avg.toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
