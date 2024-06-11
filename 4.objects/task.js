f9920b7788f0cfacca6d886c686c236d619807bc
ladislav Laptop <vladislavchs@internet.ru>
2024-06-11 17:23:04 +0300

    task 4 done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,26 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = []; 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (!this.hasOwnProperty('excluded')) {
+        this.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    return this.hasOwnProperty('marks') ? (this.marks.length !==0 ? (this.marks.reduce((acc, mark) => (acc + mark), 0) / this.marks.length) : 0) : 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
