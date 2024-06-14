623dc00a0df006012a009ffa2c53c68f5831762f
ladislav Laptop <vladislavchs@internet.ru>
2024-06-14 13:38:56 +0300

    task 3 done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2. length && arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter((user) => user.gender === gender).reduce((acc, element, index, arr) => acc + element.age / arr.length, 0);
 }
